a3bbae3e5a3acc4b72cc4202559d66ea97acfc02
Ansh Sharma <49720994+AnshSharmaa@users.noreply.github.com>
2022-02-22 21:34:09 +0530

    Fix: add BrowserRouter and Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-
 //import Header from "./components/header"
-import Cards from "./components/Cards";
+import Cards from './components/Cards'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Cards/>
-      
-    </div>
-  );
+    <Routes>
+      <Route
+        path='/'
+        element={
+          <div className='App'>
+            <Cards />
+          </div>
+        }
+      />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
